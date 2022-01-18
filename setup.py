3ee26afe72cb4dd15b00e266e1b504cb5d4075d2
vitskvara <vit.skvara@gmail.com>
2022-01-18 14:36:56 +0100

    setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sgad',
+    version='0.1.0',
+    author='Vit Skvara',
+    author_email='vit.skvara@gmail.com',
+    packages=find_packages(),
+    scripts=[],
+    url='https://github.com/vitskvara/sgad',
+    description='EFIT++ utilities',
+    long_description=open('README').read(),
+    install_requires=[]
+)
