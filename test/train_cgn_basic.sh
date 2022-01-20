011bd72bee71b8aa84ccff3b0984cb875c391aac
vitskvara <vit.skvara@gmail.com>
2022-01-20 11:11:55 +0100

    edited test run scripts

--- test/train_cgn_basic.sh
+++ test/train_cgn_basic.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
+# this is currently broken
 DIR=$(pwd)
 SGADHOME=/home/skvara/work/counterfactual_ad/sgad/sgad
 cd $SGADHOME
-python cgn/train_cgn.py --outpath $DIR/_tmp --model_name cgn_test --cfg $DIR/data/basic_cgn_cfg.yaml --target_class 1
+CUDA_LAUNCH_BLOCKING=1 python cgn/train_cgn.py --outpath $DIR/_tmp --model_name cgn_test --cfg $DIR/data/basic_cgn_cfg.yaml --target_class 1
--- test/train_cgn_other.sh
+++ test/train_cgn_other.sh
@@ -1,5 +1,2 @@
 #!/bin/bash 
-DIR=$(pwd)
-SGADHOME=/home/skvara/work/counterfactual_ad/sgad/sgad
-cd $SGADHOME
-python cgn/train_cgn_nonmnist.py --outpath $DIR/_tmp --cfg $DIR/data/wmnist_cgn_cfg.yaml --seed 1  --target_class 1
+python ../sgad/cgn/train_cgn_nonmnist.py --outpath _tmp --cfg data/wmnist_cgn_cfg.yaml --seed 1  --target_class 1
