bbd7bbac2348789195e59664d108813780d90052
vitskvara <vit.skvara@gmail.com>
2022-02-10 15:40:36 +0100

    fixes

--- sgad/cgn/models/cgn_anomaly.py
+++ sgad/cgn/models/cgn_anomaly.py
@@ -204,7 +204,7 @@ class CGNAnomaly(nn.Module):
 
                 # collect losses
                 def get_val(t):
-                    return t.data.cpu().numpy()
+                    return float(t.data.cpu().numpy())
                 niter += 1
                 losses_all['iter'].append(niter)
                 losses_all['epoch'].append(epoch)
@@ -286,10 +286,10 @@ class CGNAnomaly(nn.Module):
             save_image(x.data, path, nrow=n_rows, normalize=True, padding=2)
 
         Path(sample_path).mkdir(parents=True, exist_ok=True)
-        save(x_gen.data, f"{sample_path}/0_{batches_done:d}_x_gen.png", n_rows, sz=self.img_dim)
-        save(mask.data, f"{sample_path}/1_{batches_done:d}_mask.png", n_rows, sz=self.img_dim)
-        save(foreground.data, f"{sample_path}/2_{batches_done:d}_foreground.png", n_rows, sz=self.img_dim)
-        save(background.data, f"{sample_path}/3_{batches_done:d}_background.png", n_rows, sz=self.img_dim)
+        save(x_gen.data, f"{sample_path}/0_{batches_done:d}_x_gen.png", n_rows, sz=self.config.img_dim)
+        save(mask.data, f"{sample_path}/1_{batches_done:d}_mask.png", n_rows, sz=self.config.img_dim)
+        save(foreground.data, f"{sample_path}/2_{batches_done:d}_foreground.png", n_rows, sz=self.config.img_dim)
+        save(background.data, f"{sample_path}/3_{batches_done:d}_background.png", n_rows, sz=self.config.img_dim)
 
     def predict(self, X, score_type="discriminator", workers=12):
         if not score_type in ["discriminator", "perceptual"]:
