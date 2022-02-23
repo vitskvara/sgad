619a32b962fd0e9d417fe13c75ab2179c333a57e
Vít Škvára <vit.skvara@gmail.com>
2022-02-23 13:33:36 +0100

    wmnist denormalization

--- sgad/utils/img_utils.py
+++ sgad/utils/img_utils.py
@@ -2,12 +2,14 @@ import torch
 from torchvision.transforms import ToPILImage
 import torch.nn.functional as F
 
-def to_img(x, zoom=1):
+def to_img(x, zoom=1, denormalize=False):
     """Convert a 3D/4D array of (DxHxW)/(NxDxHxW) to a PILimage format. zoom can be int or tuple."""
+    if denormalize:
+        _x = x*0.5+0.5
     if type(zoom) == int:
         zoom = (zoom, zoom)
     t = ToPILImage()
-    _x = torch.tensor(x)
+    _x = torch.tensor(_x)
     if len(x.shape) == 4:
         _x=torch.concat([_x[i,:,:,:] for i in range(_x.shape[0])],2)
     _x = _x.reshape(1, *_x.shape)
