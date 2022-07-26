de2a617f0129758f3a2afde93f8dd56beaa652ab
Vít Škvára <vit.skvara@gmail.com>
2022-07-26 15:11:42 +0200

    sgvaegan

--- sgad/sgvae/__init__.py
+++ sgad/sgvae/__init__.py
@@ -2,6 +2,7 @@ from .vae import *
 from .gan import *
 from .sgvae import *
 from .vaegan import *
+from .sgvaegan import *
 from .utils import *
 from .alpha_model import *
 from .robust_lr import *
