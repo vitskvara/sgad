c55dd38e56f9c01d2dc0b65f00adb17c410bbc75
Vít Škvára <vit.skvara@gmail.com>
2023-02-06 11:11:29 +0100

    finished example scripts

--- scripts/sgvaegan_example/train_wmnist.py
+++ scripts/sgvaegan_example/train_wmnist.py
@@ -58,55 +58,6 @@ model.fit_alpha(tr_x, val_x, val_y, k, beta0=10.0,
             alpha0=np.array([1.0, 0, 0, 0, 0]), # sometimes this helps with convergence
             init_alpha = np.array([1.0, 1.0, 0, 0, 0]) 
     )
-tst_scores_alpha = model.predict_with_alpha(model, tst_x)
+tst_scores_alpha = model.predict_with_alpha(tst_x)
 alpha_auc = roc_auc_score(tst_y, tst_scores_alpha)
 print(f"Alpha-based AUC={alpha_auc}")
-
-
-
-
-
-
-
-
-
-from sklearn.neighbors import NearestNeighbors
-from sgad.sgvae import RobustLogisticRegression
-k = 31
-
-# alpha scores - first compute the encodings
-tr_encodings = model.encode_mean_batched(tr_x)
-val_encodings = model.encode_mean_batched(val_x)
-
-# now compute the knn scores - for each latent separately
-def knn_score(knn_model, x):
-	return knn_model.kneighbors(x)[0].mean(1)
-
-#
-knn_models = [NearestNeighbors(n_neighbors=k) for _ in range(3)]
-[m.fit(encodings, tr_y) for (m,encodings) in zip(knn_models, tr_encodings)]
-
-# now compute the alpha scores on the validation data
-rec_scores = model.reconstruction_score(val_x)
-disc_scores = model.discriminator_score(val_x)
-knn_scores = [knn_score(m, encodings) for (m,encodings) in zip(knn_models, val_encodings)]
-val_scores = np.vstack((rec_scores, disc_scores, *knn_scores)).transpose()
-
-beta = 10/sum(val_y)
-init_alpha = np.array([1.0, 1.0, 0, 0, 0]) # sometimes this helps with better convergence
-alpha0 = np.array([1.0, 0, 0, 0, 0])
-lr_model = RobustLogisticRegression(val_scores.shape[1], alpha=init_alpha, beta=beta, alpha0=alpha0)
-lr_model.fit(val_scores, val_y, scale=True, early_stopping=True)
-alpha = lr_model.alpha.detach().numpy()
-
-
-# predict
-tst_encodings = model.encode_mean_batched(tst_x)
-rec_scores = model.reconstruction_score(tst_x)
-disc_scores = model.discriminator_score(tst_x)
-knn_scores = [knn_score(m, encodings) for (m,encodings) in zip(knn_models, tst_encodings)]
-tst_scores = np.vstack((rec_scores, disc_scores, *knn_scores)).transpose()
-tst_scores = lr_model.scaler_transform(tst_scores)
-tst_scores_alpha = np.matmul(tst_scores, alpha)
-
-roc_auc_score(tst_y, tst_scores_alpha)
